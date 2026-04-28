c0370f488ed0e1ea8ae578e11e56e2f661ded567
GUILLERMOLINEROA <159136234+GUILLERMOLINEROA@users.noreply.github.com>
2026-04-28 09:51:36 -0300

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+# =============================================================================
+# utils/__init__.py
+# Archivo inicializador del paquete 'utils'.
+# Permite importar módulos como: from utils.scoring import calcular_puntos
+# =============================================================================
